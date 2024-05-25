a7dee4adaae175a03b8c81136bdd2fec025d9993
Suyog B <32979736+SuyogB@users.noreply.github.com>
2024-05-26 03:07:00 +0530

    Update openai_operations.js

--- openai_operations.js
+++ openai_operations.js
@@ -1,5 +1,5 @@
 // Import modules
-import GoogleGenerativeAI from 'generative-ai';
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 export class OpenAIOperations {
     constructor(file_context, openai_key, model_name, history_length) {
